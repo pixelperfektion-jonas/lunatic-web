467f2644fafdcb83cc12d64ae52c40c6e8a06750
pixelperfektion-jonas <jonas.iljic@pixelperfektion.de>
2024-07-11 13:56:03 +0200

    Update 110724

--- script.js
+++ script.js
@@ -1,104 +1,165 @@
-const projektloesungenSlider = new Swiper('.swiper.is-projektloesungen', {
+const projektloesungenSlider = new Swiper(".swiper.is-projektloesungen", {
   // Optional parameters
   loop: true,
   slidesPerView: 3,
   spaceBetween: 16,
+  speed: 750,
+  preventClicks: false,
+  preventClicksPropagation: false,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.25,
+    },
+    480: {
+      slidesPerView: 2.5,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-btn-next',
-    prevEl: '.swiper-btn-prev',
+    nextEl: ".swiper-btn-next",
+    prevEl: ".swiper-btn-prev",
   },
-
 });
 
-const socialproofSlider = new Swiper('.swiper.is-social-proof', {
-   // Optional parameters
+const socialproofSlider = new Swiper(".swiper.is-social-proof", {
+  // Optional parameters
   loop: true,
   slidesPerView: 1.5,
   spaceBetween: 16,
+  speed: 750,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 1.5,
+    },
+    768: {
+      slidesPerView: 1.5,
+    },
+  },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-btn-next.is-social-proof',
-    prevEl: '.swiper-btn-prev.is-social-proof',
+    nextEl: ".swiper-btn-next.is-social-proof",
+    prevEl: ".swiper-btn-prev.is-social-proof",
   },
-
 });
 
-const aboutusSlider = new Swiper('.swiper.is-aboutus', {
-   // Optional parameters  
+const aboutusSlider = new Swiper(".swiper.is-aboutus", {
+  // Optional parameters
+  preloadImages: true,
+  lazy: {
+    loadPrevNext: true,
+    loadPrevNextAmount: 3,
+  },
   loop: true,
   slidesPerView: 2.5,
   spaceBetween: 16,
+  allowTouchMove: false,
   autoplay: {
-  	delay: 0,
+    delay: 0,
     disableOnInteraction: false,
- 	},
+  },
   speed: 6000,
-  
+  breakpoints: {
+    320: {
+      slidesPerView: 1.5,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2.5,
+    },
+  },
+
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-btn-next.is-aboutus',
-    prevEl: '.swiper-btn-prev.is-aboutus',
+    nextEl: ".swiper-btn-next.is-aboutus",
+    prevEl: ".swiper-btn-prev.is-aboutus",
   },
 });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    // Alle Elemente mit der Klasse 'text-team-member-name' und dem Custom Attribute 'data-break-after' auswählen
-    var cmsItems = document.querySelectorAll('.text-team-member-name[data-break-after]');
+document.addEventListener("DOMContentLoaded", function () {
+  // Alle Elemente mit der Klasse 'text-team-member-name' und dem Custom Attribute 'data-break-after' auswählen
+  var cmsItems = document.querySelectorAll(
+    ".text-team-member-name[data-break-after]"
+  );
 
-    cmsItems.forEach(function(item) {
-      // Textinhalt des Elements erhalten
-      var text = item.innerText || item.textContent;
+  cmsItems.forEach(function (item) {
+    // Textinhalt des Elements erhalten
+    var text = item.innerText || item.textContent;
 
-      // Text in Worte aufteilen
-      var words = text.split(' ');
+    // Text in Worte aufteilen
+    var words = text.split(" ");
 
-      if (words.length > 1) {
-        // Ersten Wort und den Rest trennen
-        var firstWord = words.shift();
-        var rest = words.join(' ');
+    if (words.length > 1) {
+      // Ersten Wort und den Rest trennen
+      var firstWord = words.shift();
+      var rest = words.join(" ");
 
-        // Neuen HTML-Inhalt mit Break nach dem ersten Wort erstellen
-        item.innerHTML = firstWord + '<br>' + rest;
-      }
-    });
+      // Neuen HTML-Inhalt mit Break nach dem ersten Wort erstellen
+      item.innerHTML = firstWord + "<br>" + rest;
+    }
   });
+});
 
-const partnerSlider = new Swiper('.swiper.is-partner', {
-   // Optional parameters  
+const partnerSlider = new Swiper(".swiper.is-partner", {
+  // Optional parameters
   loop: true,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   spaceBetween: 96,
   autoplay: {
-  	delay: 0,
+    delay: 0,
     disableOnInteraction: false,
- 	},
+  },
   speed: 6000,
-  
+
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-btn-next.is-aboutus',
-    prevEl: '.swiper-btn-prev.is-aboutus',
+    nextEl: ".swiper-btn-next.is-aboutus",
+    prevEl: ".swiper-btn-prev.is-aboutus",
   },
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Wähle alle Partner-Elemente auf der Seite aus
-    var partnerElements = document.querySelectorAll('.partner');
+document.addEventListener("DOMContentLoaded", function () {
+  // Wähle alle Partner-Elemente auf der Seite aus
+  var partnerElements = document.querySelectorAll(".partner");
 
-    // Iteriere über jedes Partner-Element
-    partnerElements.forEach(function(partnerElement) {
-        // Hole den Wert des CMS-Feldes 'Logo-Skalierungsfaktor' für dieses Partner-Element
-        var scaleFactor = parseFloat(partnerElement.dataset.logoSkalierungsfaktor);
+  // Iteriere über jedes Partner-Element
+  partnerElements.forEach(function (partnerElement) {
+    // Hole den Wert des CMS-Feldes 'Logo-Skalierungsfaktor' für dieses Partner-Element
+    var scaleFactor = parseFloat(partnerElement.dataset.logoSkalierungsfaktor);
 
-        // Selektiere das Logo-Bild-Element innerhalb des aktuellen Partner-Elements
-        var logoImage = partnerElement.querySelector('.swiper-logo-img');
+    // Selektiere das Logo-Bild-Element innerhalb des aktuellen Partner-Elements
+    var logoImage = partnerElement.querySelector(".swiper-logo-img");
 
-        // Wende die Skalierung auf das Logo-Bild-Element an, falls vorhanden
-        if (logoImage) {
-            logoImage.style.transform = 'scale(' + scaleFactor + ')';
-        }
-    });
+    // Wende die Skalierung auf das Logo-Bild-Element an, falls vorhanden
+    if (logoImage) {
+      logoImage.style.transform = "scale(" + scaleFactor + ")";
+    }
+  });
 });
+
+document.getElementById("email").addEventListener("blur", function () {
+  validateEmail();
+});
+
+function validateEmail() {
+  const emailInput = document.getElementById("email");
+  const emailValue = emailInput.value;
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailPattern.test(emailValue)) {
+    emailInput.classList.remove("error");
+    emailInput.classList.add("success");
+  } else {
+    emailInput.classList.remove("success");
+    emailInput.classList.add("error");
+  }
+}
